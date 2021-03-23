065696cde3d9474cc0dd7361c16a6c55b66df424
Starford Omwakwe <starfordomwakwe@gmail.com>
2021-03-23 23:07:57 +0300

    add directive and pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
 import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
-  declarations: [AppComponent, GoalComponent, GoalDetailComponent],
+  declarations: [AppComponent, GoalComponent, GoalDetailComponent, StrikethroughDirective, DateCountPipe],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
